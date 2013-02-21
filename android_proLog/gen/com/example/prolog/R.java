072100221d262e9796120f9010609b9da745e9ec
Manuel <manask88@gmail.com>
2013-02-21 16:03:30 -0500

    first screen

--- android_proLog/gen/com/example/prolog/R.java
+++ android_proLog/gen/com/example/prolog/R.java
@@ -10,22 +10,35 @@ package com.example.prolog;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f050000;
+        public static final int grey=0x7f050002;
+        public static final int white=0x7f050001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int mainLinearLayoutMain=0x7f080000;
+        public static final int menu_settings=0x7f080002;
+        public static final int searchView=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040002;
-        public static final int menu_settings=0x7f040001;
+        public static final int addNew=0x7f040001;
+        public static final int app_name=0x7f040006;
+        public static final int contacts=0x7f040000;
+        public static final int followUps=0x7f040004;
+        public static final int hello_world=0x7f040008;
+        public static final int interactions=0x7f040003;
+        public static final int menu_settings=0x7f040007;
+        public static final int viewGroups=0x7f040005;
+        public static final int viewList=0x7f040002;
     }
     public static final class style {
         /** 
@@ -49,10 +62,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
