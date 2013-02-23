0d9aee2805d07a68fdbae91b5ba3d03cf3ed2857
rpatwa <ruchir.patwa90@gmail.com>
2013-02-23 18:27:58 -0500

    new followup activity

---
+++ src/com/example/prolog/NewFollowUpActivity.java
@@ -0,0 +1,22 @@
+package com.example.prolog;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class NewFollowUpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_follow_up);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.new_follow_up, menu);
+		return true;
+	}
+
+}
